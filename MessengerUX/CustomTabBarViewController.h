9bc94cd79a14f61e81e4246f5a5c1363236ea27f
ttphong <ttphong2608@gmail.com>
2017-03-10 17:47:38 +0700

    [add] custom tab bar

---
+++ MessengerUX/CustomTabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  CustomTabBarViewController.h
+//  MessengerUX
+//
+//  Created by CPU11808 on 3/10/17.
+//  Copyright © 2017 CPU11815. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTabBarViewController : UITabBarController
+
+@end
---
+++ MessengerUX/CustomTabBarViewController.m
@@ -0,0 +1,73 @@
+//
+//  CustomTabBarViewController.m
+//  MessengerUX
+//
+//  Created by CPU11808 on 3/10/17.
+//  Copyright © 2017 CPU11815. All rights reserved.
+//
+
+#import "CustomTabBarViewController.h"
+#import "BaseTabViewController.h"
+
+@interface CustomTabBarViewController ()
+
+@end
+
+@implementation CustomTabBarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    BaseTabViewController *homeVC = [BaseTabViewController viewControllerWithName:@"Home" andImage:[UIImage imageNamed:@"tabThumbIcon" ]];
+    
+    BaseTabViewController *callVC = [BaseTabViewController viewControllerWithName:@"Calls" andImage:[UIImage imageNamed:@"tabThumbIcon" ]];
+    
+    BaseTabViewController *cameraVC = [BaseTabViewController viewControllerWithName:@"" andImage:[UIImage imageNamed:@"tabThumbIcon" ]];
+    
+    BaseTabViewController *groupVC = [BaseTabViewController viewControllerWithName:@"Groups" andImage:[UIImage imageNamed:@"tabThumbIcon" ]];
+    
+    BaseTabViewController *peopleVC = [BaseTabViewController viewControllerWithName:@"People" andImage:[UIImage imageNamed:@"tabThumbIcon" ]];
+    
+    self.viewControllers = @[homeVC, callVC, cameraVC, groupVC, peopleVC];
+    
+    [self setupMiddleButton];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)setupMiddleButton{
+    
+    UIButton *middleButton = [[UIButton alloc] init];
+    middleButton.frame = CGRectMake(0, 0, 64, 64);
+    
+    CGRect buttonFrame = middleButton.frame;
+    buttonFrame.origin.y = self.view.bounds.size.height - buttonFrame.size.height - 16;
+    buttonFrame.origin.x = self.view.bounds.size.width / 2 - buttonFrame.size.width / 2;
+    middleButton.frame = buttonFrame;
+    
+    middleButton.backgroundColor = [UIColor redColor];
+    middleButton.layer.cornerRadius = buttonFrame.size.height / 2;
+    [self.view addSubview:middleButton];
+    
+    [middleButton setImage:[UIImage imageNamed:@""] forState:UIControlStateNormal];
+    [middleButton addTarget:self action:@selector(middleButtonAction) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void)middleButtonAction {
+    NSLog(@"button pressed");
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
